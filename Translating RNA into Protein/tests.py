259b2f0a2227b6e80ca73395fb04cdef041cf969
reflash <reflash-ne@yandex.ru>
2017-06-06 16:41:19 +0300

    Solved rna translation task

---
+++ Translating RNA into Protein/tests.py
@@ -0,0 +1,15 @@
+import unittest
+from main import decode
+
+# python -m unittest tests
+class MendelLawTestCase(unittest.TestCase):
+
+    def test_input(self):
+        rna = 'AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA'
+        self.assertEqual(decode(rna), 'MAMAPRTEINSTRING')
+
+    def test_file(self):
+        f = open('input.txt', 'r')
+        rna = f.read().strip()
+        print(decode(rna))
+        f.close()
