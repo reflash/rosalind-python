6bfd8764c32776604bb1e42bb43eabe367a41d08
reflash <reflash-ne@yandex.ru>
2017-06-07 11:53:21 +0300

    Deleted comment

--- Finding a Motif in DNA/main.py
+++ Finding a Motif in DNA/main.py
@@ -1,7 +1,6 @@
 from functional import seq
 
 def positions(code,s_code):
-    # split by 3 and delete last codon - cause its always stop codon
     n = len(s_code)
     pos = (seq([(i+1) if code[i:i+n] == s_code else None
                         for i in range(len(code))])
