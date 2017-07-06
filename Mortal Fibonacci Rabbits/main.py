2eaaba668c410078d388afcbcd441e797f75e620
reflash <reflash-ne@yandex.ru>
2017-07-06 11:23:46 +0300

    Solved mortal rabbits task

--- Mortal Fibonacci Rabbits/main.py
+++ Mortal Fibonacci Rabbits/main.py
@@ -1,40 +1,13 @@
-from functional import seq
-import collections
+def dead_pairs(s, i, m):
+    if i < m:
+        return 0
+    elif i == m:
+        return 1
+    else:
+        return s[i - (m + 1)]
 
-class memoized(object):
-    '''Decorator. Caches a function's return value each time it is called.
-    If called later with the same arguments, the cached value is returned
-    (not reevaluated).
-    '''
-    def __init__(self, func):
-        self.func = func
-        self.cache = {}
-
-    def __call__(self, *args):
-        if not isinstance(args, collections.Hashable):
-            # uncacheable. a list, for instance.
-            # better to not cache than blow up.
-            return self.func(*args)
-        if args in self.cache:
-            return self.cache[args]
-        else:
-            value = self.func(*args)
-            self.cache[args] = value
-            return value
-
-    def __repr__(self):
-        '''Return the function's docstring.'''
-        return self.func.__doc__
-
-    def __get__(self, obj, objtype):
-        '''Support instance methods.'''
-        return functools.partial(self.__call__, obj)
-
-@memoized
-def fib(n,k):
-    if n <= 0:
-        return 0;
-    return n if n <= 1 else fib(n-1,k) + fib(n-2,k) - fib(n-k-1,k)
-
-def total(n,m):
-    return fib(n,m)
+def rabbit_pairs(n, m):
+    s = list([1,1])
+    for i in range(2,n):
+        s.append(s[i - 1] + s[i - 2] - dead_pairs(s, i, m))
+    return s[-1]
--- Mortal Fibonacci Rabbits/tests.py
+++ Mortal Fibonacci Rabbits/tests.py
@@ -1,15 +1,15 @@
 import unittest
-from main import total
+from main import rabbit_pairs
 
 # python -m unittest tests
 class MortalFibTestCase(unittest.TestCase):
 
     def test_input(self):
         n,m = 6,3
-        self.assertEqual(total(n,m), 4)
+        self.assertEqual(rabbit_pairs(n,m), 4)
 
     def test_file(self):
         f = open('input.txt', 'r')
         n,m = map(int, f.read().strip().split())
-        print(total(n,m))
+        print(rabbit_pairs(n,m))
         f.close()
